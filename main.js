0ebe69bc9d926c39b40dedf7fb17d19fab22da38
Matheus Ferreira <codeswan122016@gmail.com>
2023-08-17 08:27:39 -0300

    modificando o array

--- main.js
+++ main.js
@@ -1,37 +1,31 @@
-const alunos = []
-const alunos2 = alunos.map(function(nomeDoAluno, notaDoAluno) {
-    return {
-        nome: nomeDoAluno,
-        nota: notaDoAluno,
+const alunos = [
+    {
+        nome: 'Matheus',
+        nota: '6'
+    },
+    {
+        nome: 'Joao',
+        nota: '7'
+    },
+    {
+        nome: 'Maria',
+        nota: '3'
+    },
+    {
+        nome: 'Paulo',
+        nota: '5'
+    },
+    {
+        nome: 'Lara',
+        nota: '10'
+    },
+    {
+        nome: 'Luiz',
+        nota: '2'
     }
-})
+]
 
-alunos2.push({
-    nome: 'Matheus',
-    nota: '6'
-})
-alunos2.push({
-    nome: 'Joao',
-    nota: '7'
-})
-alunos2.push({
-    nome: 'Maria',
-    nota: '3'
-})
-alunos2.push({
-    nome: 'Paulo',
-    nota: '5'
-})
-alunos2.push({
-    nome: 'Lara',
-    nota: '10'
-})
-alunos2.push({
-    nome: 'Luiz',
-    nota: '2'
-})
-
-const alunoNaMedia = alunos2.filter(function(item) {
+const alunoNaMedia = alunos.filter(function(item) {
     return item.nota >= 6;
 })
 console.log(alunoNaMedia);
